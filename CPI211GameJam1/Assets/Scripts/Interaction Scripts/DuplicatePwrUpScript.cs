6e4c91045053cbbea004904775dc63e0a07abde6
Muab-Dib <jpsteph1@asu.edu>
2024-09-14 14:18:16 -0700

    Pinball can't leave map anymore, and new power
    Pinball used to be able to bounce out of the arena easily, so I made a glass roof for the aesthetic (though it wasn't fully capable of keeping the ball in). And I set the Y position as static, meaning it can't change, thus the ball can't bounce up out of the arena. Plus, I added walls so it can't bounce out either.
    
    Lastly, I added a duplicate power up script so that we can get two balls now! Also added weight and drag so the pinball should behave as you'd expect irl.

---
+++ CPI211GameJam1/Assets/Scripts/Interaction Scripts/DuplicatePwrUpScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuplicatePwrUpScript : MonoBehaviour
+{
+    public GameObject DupBallPwrUpObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            for(int i = 0; i < 1; i++)
+            {
+                Instantiate(other.gameObject);
+            }
+            
+            DupBallPwrUpObj.SetActive(false);
+        }
+    }
+}

468bbc61ca49bb563b205d529264c2e4a63bda87
camilee03 <crlee15@asu.edu>
2024-09-14 15:15:50 -0700

    Level 1 Design

--- CPI211GameJam1/Assets/Scripts/PinballMachine/Slots.cs
+++ CPI211GameJam1/Assets/Scripts/PinballMachine/Slots.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Slots : MonoBehaviour
 {
     [SerializeField] int score;
+    [SerializeField] bool debugLog;
+
+    private void Update()
+    {
+        Debug.unityLogger.logEnabled = debugLog;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Pinball")
         {
             other.gameObject.SetActive(false);
             // Add score
+            Debug.Log("Score!");
         }
     }
 }
